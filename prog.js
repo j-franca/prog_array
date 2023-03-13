43d2a8837d6a7e1cc84ff0e245863e377eace326
Júlia <julia_franca1@estudante.sesisenai.org.br>
2023-03-13 08:23:44 -0300

    js criado e css modificado

---
+++ prog.js
@@ -0,0 +1,7 @@
+    let res = document.getElementById('res')
+    let nome_cidade = []
+
+function inserir(){
+    let cidade = document.getElementById('cidade').value
+    nome_cidade.push(cidade)
+}
